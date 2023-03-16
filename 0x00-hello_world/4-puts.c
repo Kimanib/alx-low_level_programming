dcfa11a77bf0c82367fe8e98336c759e3d16e3d4
KImaniB <abrwyan7@gmail.com>
2023-03-16 11:05:22 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * description: main - prins out a string to stdout
+ * Return: 0 if success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
