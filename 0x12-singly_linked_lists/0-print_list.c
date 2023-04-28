339d186208106421761e776a086097babad7efe3
kimanib <abrwyan7@gmail.com>
2023-04-27 21:40:53 -0700

    new corrections of the nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 
 /**
- * list_len - finds number of elements in linked list
- * @h: pointer to the list
- * Return: number of elements
+ * print_list - prints all the elements of a list_t list
+ * @h: list's name
+ * Return: number of nodes
  */
-
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
-	size_t elements = 0;
+	size_t i = 0;
 
 	while (h)
 	{
-		elements++;
+		if (h->str == 0)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
 		h = h->next;
-	}
 
-	return (elements);
+	}
+	return (i);
 }
